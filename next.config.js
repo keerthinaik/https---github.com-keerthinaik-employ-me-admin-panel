20da2466868ccf4073318d2bfb9372479417ccd9
Keerthinaik R <keerthinaikrr15@gmail.com>
2025-07-05 14:31:17 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ next.config.js
@@ -0,0 +1,45 @@
+/** @type {import('next').NextConfig} */
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://148.72.244.169:3000';
+
+const apiURL = new URL(API_BASE_URL);
+
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: apiURL.protocol.replace(':', ''),
+        hostname: apiURL.hostname,
+        port: apiURL.port,
+        pathname: '/**',
+      },
+    ],
+  },
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/v1/:path*',
+        destination: `${API_BASE_URL}/api/v1/:path*`,
+      },
+      {
+        source: '/api/location/:path*',
+        destination: `${API_BASE_URL}/api/location/:path*`,
+      },
+    ]
+  },
+};
+
+module.exports = nextConfig;
