c7a3a303786d41ade9f9adf4ecf202e6a9b2d9a3
Keerthinaik R <keerthinaikrr15@gmail.com>
2025-07-05 10:04:41 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/rich-text-editor.tsx
+++ src/components/rich-text-editor.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import { cn } from '@/lib/utils';
 
 interface RichTextEditorProps {
   value: string;
